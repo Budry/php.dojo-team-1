ad1c31b4473e8b353be9e4644875d3bed009f4ae
Ondřej Záruba <zarubaondra@gmail.com>
2023-02-03 07:18:43 +0100

    Update

--- src/Main.php
+++ src/Main.php
@@ -9,14 +9,64 @@ class Main
         if ($number == "") {
             return "0";
         }
+        if (str_ends_with($number, ",")) {
+            return "Number expected but EOF found";
+        }
+
+        $regex = "~//(\[.+?\])+\n(.+)~";
+        preg_match($regex, $number, $matches);
+        if (!empty($matches)) {
+            $separator = $matches[1];
+
+            $pos = strpos($matches[2], ",");
+
+            if ($pos !== false) {
+                return "'|' expected, but ',' found at position " . $pos;
+            }
+
+            $parts = explode($separator, $matches[2]);
+
+            $negatives = [];
+            foreach ($parts as $part) {
+                if ($part < 0) {
+                    $negatives[] = $part;
+                }
+            }
+
+            if (!empty($negatives)) {
+                return "Negative not allowed : " . implode(",", $negatives);
+            }
+
+            $parts = array_filter($parts, fn ($item) => $item <= 1000);
+
+            return (string)array_sum($parts);
+        }
+
+        $fuk = strpos($number, ",\n");
+        if ($fuk != false) {
+            return "Invalid input on pos: " . $fuk;
+        }
 
         $parts = explode(",", $number);
-		$out = [];
+        $out = [];
 
-		foreach ($parts as $part) {
-			$out = array_merge($out, explode("\n", $part));
-		}
+        foreach ($parts as $part) {
+            $out = array_merge($out, explode("\n", $part));
+        }
 
-        return (string) array_sum($out);
+        $out = array_filter($out, fn ($item) => $item <= 1000);
+
+        $negatives = [];
+        foreach ($out as $x) {
+            if ($x < 0) {
+                $negatives[] = $x;
+            }
+        }
+        if ($negatives) {
+            return "Negative not allowed : " . implode(",", $negatives);
+        }
+
+
+        return (string)array_sum($out);
     }
 }
--- tests/MainTest.php
+++ tests/MainTest.php
@@ -17,6 +17,13 @@ class MainTest extends TestCase
         $this->assertEquals("3.3", $main->add("1.1, 2.2"));
         $this->assertEquals("3.3", $main->add("1.1\n2.2"));
 		$this->assertEquals("Invalid input on pos: 3", $main->add("1.1,\n2.2"));
+		$this->assertEquals("Number expected but EOF found", $main->add("2,3,"));
+		$this->assertEquals("3", $main->add("//[;]\n1;2"));
+		$this->assertEquals("'|' expected, but ',' found at position 3", $main->add("//[|]\n1|2,3"));
+        $this->assertEquals("Negative not allowed : -5", $main->add("3,-5,1"));
+        $this->assertEquals("3", $main->add("2,1001,1"));
+        $this->assertEquals("6", $main->add("//[***]\n1***2***3"));
+        $this->assertEquals("6", $main->add("//[*][%]\n1*2%3"));
     }
 
 }
